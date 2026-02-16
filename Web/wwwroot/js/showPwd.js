2b587471a0f44fca1f56b534719953926e4e773b
MateusAFreitas <mateus.freitas@fikdigital.com.br>
2026-02-16 09:25:58 -0300

    fix: mostrar senha tela de login

---
+++ Web/wwwroot/js/showPwd.js
@@ -0,0 +1,14 @@
+﻿let timeoutId;
+
+function showPassword(inputElement, button) {
+    if (inputElement.type === 'password') {
+        inputElement.type = 'text';
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function () {
+            inputElement.type = 'password';
+        }, 5000);
+    } else {
+        inputElement.type = 'password';
+        clearTimeout(timeoutId);
+    }
+}
